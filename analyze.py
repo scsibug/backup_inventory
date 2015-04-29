f5ddbfda5546b730a645b2d003cd6ea7b5f5c3ca
Greg Heartsfield <scsibug@imap.cc>
2015-04-29 18:46:58 -0500

    Analyze script now shows total size.

--- analyze.py
+++ analyze.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import psycopg2
-
+import sys
 database_name = sys.argv[1]
 database_user = sys.argv[2]
 
@@ -11,5 +11,6 @@ conn = psycopg2.connect(database=database_name, user=database_user)
 # snapshot across all roots?
 with conn.cursor() as cur:
     cur.execute("select sum(filesize) from (select items.id, root.path, files.rel_path, items.modified, items.filesize as filesize, root.rep_factor  from latest_inventory_runs runs, inventory_items items, file_references files, inventory_roots root where items.inventory_run = runs.id and items.file=files.id and runs.root_path=root.id ) fs")
-   total_size = cur.fetchone()[0]
-   print "Total Size: %.2f GB" % (total_size/(1024*1024*1024))
+    result = cur.fetchone()
+    total_size = result[0]
+    print "Total Size: %.2f GB" % (total_size/(1024*1024*1024))
