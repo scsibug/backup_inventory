8838e0321d8b28a4f54671f8238473a3af15b23e
Greg Heartsfield <scsibug@imap.cc>
2015-04-29 19:10:31 -0500

    Added summary stats to analysis output.

--- analyze.py
+++ analyze.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 import psycopg2
 import sys
+import locale
+
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
 database_name = sys.argv[1]
 database_user = sys.argv[2]
 
@@ -10,7 +14,16 @@ conn = psycopg2.connect(database=database_name, user=database_user)
 # How many files, hashes, and what is the overall size of the latest
 # snapshot across all roots?
 with conn.cursor() as cur:
-    cur.execute("select sum(filesize) from (select items.id, root.path, files.rel_path, items.modified, items.filesize as filesize, root.rep_factor  from latest_inventory_runs runs, inventory_items items, file_references files, inventory_roots root where items.inventory_run = runs.id and items.file=files.id and runs.root_path=root.id ) fs")
-    result = cur.fetchone()
-    total_size = result[0]
+    cur.execute("""SELECT sum(filesize), count(distinct(hash)), count(distinct(file)), count(distinct(i.id)) FROM
+                latest_inventory_runs lr INNER JOIN inventory_items i ON lr.id=i.inventory_run""")
+    results = cur.fetchone()
+    total_size = results[0]
+    distinct_hashes = results[1]
+    distinct_files = results[2]
+    total_items = results[3]
     print "Total Size: %.2f GB" % (total_size/(1024*1024*1024))
+    print "Total Items: %s" % locale.format("%d", total_items, grouping=True)
+    print "Distinct Relative Paths: %s" % locale.format("%d", distinct_files, grouping=True)
+    print "Distinct Hashes: %s" % locale.format("%d", distinct_hashes, grouping=True)
+
+    
