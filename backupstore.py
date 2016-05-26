79747dbf8f5bb9411945b7837f6975f6366ad111
Greg Heartsfield <scsibug@imap.cc>
2016-05-25 21:52:50 -0500

    Revamp test cases.

--- backupstore.py
+++ backupstore.py
@@ -16,73 +16,74 @@ file_refs_t = "file_references"
 # Mapping between an inventory run, and filenames/hashes.
 inventory_items_t = "inventory_items"
 
+class BackupStore:
+    def __init__(self, storename):
+        self.conn = sqlite3.connect(storename, detect_types=sqlite3.PARSE_DECLTYPES)
 
-def new_conn(filename):
-    return sqlite3.connect(filename, detect_types=sqlite3.PARSE_DECLTYPES)
+    def __enter__(self):
+        return self
 
-def close_conn(conn):
-    conn.close()
+    def __exit__(self):
+        self.conn.close()
 
-def is_db_empty(conn):
-    try:
-        current_db_version(conn)
-        return False
-    except:
-        return True
+    def is_db_empty(self):
+        try:
+            self.current_db_version()
+            return False
+        except:
+            return True
 
-def is_db_current(conn):
-    return (current_db_version(conn) == db_version)
+    def is_db_current(self):
+        return (self.current_db_version() == db_version)
 
-def current_db_version(conn):
-    c = conn.cursor()
-    vr = c.execute('SELECT version FROM {bt}'.format(bt=backup_md_t)).fetchone()
-    return vr[0]
+    def current_db_version(self):
+        c = self.conn.cursor()
+        vr = c.execute('SELECT version FROM {bt}'.format(bt=backup_md_t)).fetchone()
+        return vr[0]
 
     # Create database tables, assumes the database did not previously exist.
-def initialize_database(conn):
-    c = conn.cursor()
-    # Table for metadata.  Just stores database version for now.
-    c.execute('CREATE TABLE {bt} (version INTEGER)'.format(bt=backup_md_t))
-    c.execute('INSERT INTO {bt} (version) VALUES ({v})'.format(bt=backup_md_t, v=db_version))
+    def initialize_database(self):
+        c = self.conn.cursor()
+        # Table for metadata.  Just stores database version for now.
+        c.execute('CREATE TABLE {bt} (version INTEGER)'.format(bt=backup_md_t))
+        c.execute('INSERT INTO {bt} (version) VALUES ({v})'.format(bt=backup_md_t, v=db_version))
 
-    c.execute('CREATE TABLE {ir} (id INTEGER PRIMARY KEY,'
-              'uuid TEXT, name TEXT, path TEXT, description TEXT,'
-              'type TEXT)'.format(ir=inv_roots_t))
+        c.execute('CREATE TABLE {ir} (id INTEGER PRIMARY KEY,'
+                  'uuid TEXT, name TEXT, path TEXT, description TEXT,'
+                  'type TEXT)'.format(ir=inv_roots_t))
 
-    c.execute('CREATE TABLE {iru} (id INTEGER PRIMARY KEY,'
-              'root_path INTEGER, tstamp TIMESTAMP)'.format(iru=inventory_runs_t))
+        c.execute('CREATE TABLE {iru} (id INTEGER PRIMARY KEY,'
+                  'root_path INTEGER, tstamp TIMESTAMP)'.format(iru=inventory_runs_t))
 
-    c.execute('CREATE TABLE {h} (id INTEGER PRIMARY KEY,'
-              'hash TEXT)'.format(h=hashes_t))
+        c.execute('CREATE TABLE {h} (id INTEGER PRIMARY KEY,'
+                  'hash TEXT)'.format(h=hashes_t))
 
-    c.execute('CREATE TABLE {fr} (id INTEGER PRIMARY KEY,'
-              'rel_path TEXT)'.format(fr=file_refs_t))
+        c.execute('CREATE TABLE {fr} (id INTEGER PRIMARY KEY,'
+                  'rel_path TEXT)'.format(fr=file_refs_t))
 
-    c.execute('CREATE TABLE {ii} (id INTEGER PRIMARY KEY,'
-              'inventory_run INTEGER REFERENCES {iru}(id),'
-              'hash INTEGER REFERENCES {h}(id),'
-              'file INTEGER REFERENCES {fr}(id),'
-              'modified TIMESTAMP,'
-              'filesize INTEGER)'
-              .format(ii=inventory_items_t,
-                      iru=inventory_runs_t,
-                      h=hashes_t,
-                      fr=file_refs_t))
-    # Create indexes
-    c.execute('CREATE UNIQUE INDEX hash_idx ON {h}(hash)'.format(h=hashes_t))
-    c.execute('CREATE UNIQUE INDEX file_idx ON {fr}(rel_path)'.format(fr=file_refs_t))
-    c.execute('CREATE INDEX inventory_items_hash ON {ii}(hash)'.format(ii=inventory_items_t))
-    c.execute('CREATE INDEX inventory_items_file ON {ii}(file)'.format(ii=inventory_items_t))
-    # Path data (read from config file)
-    # Snapshot of a path (files, modification dates, sizes, hash values)
-    # Disk data (UUID, name)
-    # Hash values backed up to a disk.
-    conn.commit()
+        c.execute('CREATE TABLE {ii} (id INTEGER PRIMARY KEY,'
+                  'inventory_run INTEGER REFERENCES {iru}(id),'
+                  'hash INTEGER REFERENCES {h}(id),'
+                  'file INTEGER REFERENCES {fr}(id),'
+                  'modified TIMESTAMP,'
+                  'filesize INTEGER)'
+                  .format(ii=inventory_items_t,
+                          iru=inventory_runs_t,
+                          h=hashes_t,
+                          fr=file_refs_t))
     
-def ready_database(conn):
-    if (is_db_empty(conn)):
-        initialize_database(conn)
-    elif (is_db_current(conn)):
-        pass
-    else:
-        raise Error("Database migration needed, but none have been defined")
+        # Create indexes
+        c.execute('CREATE UNIQUE INDEX hash_idx ON {h}(hash)'.format(h=hashes_t))
+        c.execute('CREATE UNIQUE INDEX file_idx ON {fr}(rel_path)'.format(fr=file_refs_t))
+        c.execute('CREATE INDEX inventory_items_hash ON {ii}(hash)'.format(ii=inventory_items_t))
+        c.execute('CREATE INDEX inventory_items_file ON {ii}(file)'.format(ii=inventory_items_t))
+    
+        self.conn.commit()
+    
+    def ready_database(self):
+        if (self.is_db_empty()):
+            self.initialize_database()
+        elif (self.is_db_current()):
+            pass
+        else:
+            raise Error("Database migration needed, but none have been defined")
--- backupstore_test.py
+++ backupstore_test.py
@@ -1,25 +1,35 @@
 import unittest
 import backupstore
+from contextlib import contextmanager
+
+@contextmanager
+def backupstore_manager(storename):
+    yield backupstore.BackupStore(storename)
 
 class TestInitialize(unittest.TestCase):
-    def setUp(self):
-        self.f = ':memory:'
-        self.conn = backupstore.new_conn(self.f)
+#    def setUp(self):
+#        self.f = ':memory:'
+#        self.bs = backupstore.BackupStore(self.f)
 
-    def tearDown(self):
-        backupstore.close_conn(self.conn)
+#    def tearDown(self):
+#        self.bs.close_conn()
+
+    def run(self, result=None):
+        with backupstore_manager(':memory:') as store:
+            self.store = store
+            super(TestInitialize, self).run(result)
 
     def test_create(self):
-        backupstore.initialize_database(self.conn)
+        self.store.initialize_database()
 
     def test_current_version(self):
-        backupstore.initialize_database(self.conn)
-        self.assertTrue(backupstore.is_db_current)
+        self.store.initialize_database()
+        self.assertTrue(self.store.is_db_current())
 
     def test_ready_new_database(self):
-        self.assertTrue(backupstore.is_db_empty(self.conn))
-        backupstore.ready_database(self.conn)
-        self.assertTrue(backupstore.is_db_current(self.conn))
+        self.assertTrue(self.store.is_db_empty())
+        self.store.ready_database()
+        self.assertTrue(self.store.is_db_current())
 
         
 if __name__ == '__main__':
